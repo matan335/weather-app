d97448e5fa71188b73229885ea5965b157981399
matan <matan@dualitiia.com>
2019-10-21 22:34:41 +0300

    Adding files.

---
+++ dist/precache-manifest.0893f6e7a47f8d057e3d7436df991801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020e83a0b01b6c292597",
+    "url": "/css/app.f60c6257.css"
+  },
+  {
+    "revision": "f33b90c029b2097c27e4",
+    "url": "/css/chunk-vendors.ac52170f.css"
+  },
+  {
+    "revision": "c7ec2e2fe2e4c1b1f9d0950a66c34f36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "020e83a0b01b6c292597",
+    "url": "/js/app.1e19f790.js"
+  },
+  {
+    "revision": "f33b90c029b2097c27e4",
+    "url": "/js/chunk-vendors.be946bab.js"
+  },
+  {
+    "revision": "9d035edb4c3246ff9e9fed4aee929099",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
